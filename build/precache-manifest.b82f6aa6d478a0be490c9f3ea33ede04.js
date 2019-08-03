9c27968878ffdb83a0e52a0b5dbcd19d9dc817de
RomanOt <vatokato@gmail.com>
2019-08-03 13:51:17 +0500

    dep fix

---
+++ build/precache-manifest.b82f6aa6d478a0be490c9f3ea33ede04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba149d6daa79380b9b531c274d5a4583",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a83a136bcdb505266ce",
+    "url": "./static/css/main.6293d0fc.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1a83a136bcdb505266ce",
+    "url": "./static/js/main.c63e42cc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
